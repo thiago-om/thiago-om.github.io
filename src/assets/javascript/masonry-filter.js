7f1cbd3eb9559ac7bcf042328d1a81a87d68e6e5
Thiago Om <thiago@xingur.com>
2015-01-30 18:15:22 +0000

    tidy up

--- src/assets/javascript/masonry-filter.js
+++ src/assets/javascript/masonry-filter.js
@@ -1,50 +1,50 @@
-jQuery(document).ready(function ($) {
+$(document).ready(function ($) {
 
-    var $container = $('.grid').masonry({
-        columnWidth: 280,
-        itemSelector: '.item',
-        isFitWidth: true
-    });
+  var $container = $('.grid').masonry({
+    columnWidth: 280,
+    itemSelector: '.item',
+    isFitWidth: true
+  });
 
-    var arrThought = new Array($('.thought'));
-    var arrCreative = new Array($('.creative'));
-    var arrWork = new Array($('.work'));
+  var arrThought = new Array($('.thought'));
+  var arrCreative = new Array($('.creative'));
+  var arrWork = new Array($('.work'));
 
-    $('input').click(function () {
+  $('input').click(function () {
 
 
-        if ($('.thought-filter').is(':checked')) {
-            $container.masonry('hide', arrCreative.concat(arrWork));
-            $(arrThought).each(function (index, element) {
-                element.show();
-            });
-            $container.masonry();
+    if ($('.thought-filter').is(':checked')) {
+      $container.masonry('hide', arrCreative.concat(arrWork));
+      $(arrThought).each(function (index, element) {
+        element.show();
+      });
+      $container.masonry();
 
-        } else if ($('.creative-filter').is(':checked')) {
-            $container.masonry('hide', arrWork.concat(arrThought));
-            $(arrCreative).each(function (index, element) {
-                element.show();
-            });
-            $container.masonry();
+    } else if ($('.creative-filter').is(':checked')) {
+        $container.masonry('hide', arrWork.concat(arrThought));
+        $(arrCreative).each(function (index, element) {
+          element.show();
+        });
+        $container.masonry();
 
-        } else if ($('.work-filter').is(':checked')) {
-            $container.masonry('hide', arrCreative.concat(arrThought));
-            $(arrWork).each(function (index, element) {
-                element.show();
-            });
-            $container.masonry();
+    } else if ($('.work-filter').is(':checked')) {
+        $container.masonry('hide', arrCreative.concat(arrThought));
+        $(arrWork).each(function (index, element) {
+          element.show();
+        });
+        $container.masonry();
 
-        } else if ($('.all').is(':checked')) {
-            $(arrThought).each(function (index, element) {
-                element.show();
-            });
-            $(arrCreative).each(function (index, element) {
-                element.show();
-            });
-            $(arrWork).each(function (index, element) {
-                element.show();
-            });
-            $container.masonry();
-        }
-    });
+    } else if ($('.all').is(':checked')) {
+        $(arrThought).each(function (index, element) {
+          element.show();
+        });
+        $(arrCreative).each(function (index, element) {
+          element.show();
+        });
+        $(arrWork).each(function (index, element) {
+          element.show();
+        });
+        $container.masonry();
+    }
+  });
 });
