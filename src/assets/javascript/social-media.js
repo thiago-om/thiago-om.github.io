b22bd990ec1a89f74c44128c07efb5ab119256c8
Thiago de Bastos <thiago@thiagodebastos.com>
2015-02-21 02:08:37 +0000

    resolve google analytics

--- src/assets/javascript/social-media.js
+++ src/assets/javascript/social-media.js
@@ -4,7 +4,7 @@
     if (d.getElementById(id)) return;
     js = d.createElement(s);
     js.id = id;
-    js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.0";
+    js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&appId=880683755284997&version=v2.0";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
