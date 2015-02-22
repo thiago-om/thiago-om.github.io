b35cd7dbed22a6e939796d94e446586322bf503c
Thiago de Bastos <thiago@thiagodebastos.com>
2015-02-22 12:59:16 +0000

    remove fb and google to isolate twitter button failure

--- src/assets/javascript/social-media.js
+++ src/assets/javascript/social-media.js
@@ -1,13 +1,3 @@
-// facebook
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&appId=880683755284997&version=v2.0";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
 // twitter
 ! function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0],
@@ -19,13 +9,4 @@
         fjs.parentNode.insertBefore(js, fjs);
     }
 }(document, 'script', 'twitter-wjs');
-
-// google +1
-window.___gcfg = {lang: 'en-GB', parsetags: 'onload'};
-
-  (function() {
-    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = 'https://apis.google.com/js/platform.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-  })();
 
